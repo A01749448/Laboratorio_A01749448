538d0cbe6d39c42c5ba5075b4d2f5f4d826d9892
A01749448 <a01749448@itesm.mx>
2022-03-23 09:30:58 -0600

    Versión 2

---
+++ Programa.c
@@ -0,0 +1,19 @@
+{\rtf1\ansi\ansicpg1252\cocoartf2638
+\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
+{\colortbl;\red255\green255\blue255;\red239\green239\blue239;\red0\green0\blue0;}
+{\*\expandedcolortbl;;\cssrgb\c94902\c94902\c94902;\cssrgb\c0\c0\c0;}
+\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs29\fsmilli14843 \cf0 \cb2 \expnd0\expndtw0\kerning0
+#include <stdio.h>  \
+#include <stdlib.h>  \
+#include <math.h>    \
+int main()\{      i\
+nt length;      \
+scanf("%i", &length);        \
+int office = pow(2,length);      \
+printf("%i",office);      \
+return 0;  \
+\} }
